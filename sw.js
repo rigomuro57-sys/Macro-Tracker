32e51c58a0013b66c09d0559197139b8db0e30a0
rigomuro57-sys <rigomuro57@gmail.com>
2026-02-11 11:15:36 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="macro-pwa-elite-ondevice-v1";
+const CACHE="macro-pwa-elite-ondevice-v2";
 const ASSETS=["./","./index.html","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim();});
